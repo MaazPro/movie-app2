d9843c7afb1b20f0788670221e939bd0dc0eba13
MaazPro <maazmuhammad94@gmail.com>
2023-09-13 23:03:31 +0800

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavorites';
+const App = () =>{
+  const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-function App() {
+  const getMovieRequest = async (searchValue) =>{
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=8eb76a09`;
+
+    const response = await fetch (url);
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  }
+  
+  useEffect(()=>{
+    getMovieRequest(searchValue);
+  },[searchValue]);
+
+  const addFavouriteMovie = (movie) =>{
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+      </div>
     </div>
   );
-}
-
+};
 export default App;
